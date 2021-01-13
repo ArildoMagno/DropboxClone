2a5d62a2434fe01ac56dd8d3bbaf26923dc44616
Arildo <0040117@academico.ifmg.edu.br>
2021-01-13 09:16:38 -0300

    New Components

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+  Container,
+  HeaderWrapper,
+  Header,
+  DropboxLogo,
+  Content,
+} from './styles';
+
+interface Props {
+  variant: 'blue' | 'beige' | 'white' | 'black';
+  title: string;
+  description: string;
+}
+
+const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random());
+
+  function handleToggle() {
+    //if (window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+
+  return (
+      //container que cobre tudo
+      //wrapper que fica em volta
+      //header a parte de cima
+    <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+
+          <button onClick={handleToggle}>
+            {buttonVariant === 0 ? 'Interagir' : 'Acessar'}
+          </button>
+        </Header>
+      </HeaderWrapper>
+
+      <Content>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Content>
+    </Container>
+  );
+};
+
+export default Section;
